0514e6803c5eea8275bbf90cda78cdc0af07dc18
colorbox <colorbox222@gmail.com>
2019-04-30 22:11:06 +0900

    b(200) 10:50

---
+++ abc/122/b.js
@@ -0,0 +1,23 @@
+function main(input) {
+  const formatted_input = input.split('\n')[0];
+  const S = formatted_input;
+ 
+  var result = 0;
+  var tmp = 0;
+  for(var i=0;i<S.split('').length;i++){
+    var b=S.split('')[i];
+ 
+    if(b=='A' || b=='T' || b=='G' || b=='C'){
+      tmp++;
+    }else{
+      result = Math.max(result, tmp);
+      tmp=0;
+    }
+  }
+  result = Math.max(result, tmp);
+ 
+  console.log(result);
+}
+ 
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
