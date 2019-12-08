00d7eb43fc2e47535646281aa32252240dec548d
colorbox <colorbox222@gmail.com>
2019-12-08 18:04:14 +0900

    Practice D

--- abc/095/d.cpp
+++ abc/095/d.cpp
@@ -7,55 +7,40 @@ using namespace std;
 
 int main(){
   ll n,c;cin>>n>>c;
-  ll dists[n],cals[n];
-  ll n_cals[n], r_cals[n], n_cal_uppers[n], r_cal_uppers[n];
-  ll result=0;
-  n_cal_uppers[0]=-1*(1LL<<50), r_cal_uppers[0]=-1*(1LL<<50);
+  ll dists[n], r_dists[n], cals[n], r_cals[n];
   rep(i,n){
     cin>>dists[i]>>cals[i];
+    r_cals[n-1-i]=cals[i];
   }
+  rep(i,n)r_dists[i]=c-dists[n-1-i];
 
-  n_cals[0]=cals[0];
-  r_cals[0]=cals[n-1];
   rep(i,n-1){
-    n_cals[i+1]=n_cals[i]+cals[i+1];
-    r_cals[i+1]=r_cals[i]+cals[n-1-(i+1)];
+    cals[i+1]=cals[i+1]+cals[i];
+    r_cals[i+1]=r_cals[i+1]+r_cals[i];
   }
   rep(i,n){
-    n_cals[i]-=dists[i];
-    r_cals[i]-=(c-dists[n-1-i]);
+    cals[i]-=dists[i];
+    r_cals[i]-=r_dists[i];
   }
-
-  n_cal_uppers[0]=max(0LL, n_cals[0]);
-  r_cal_uppers[0]=max(0LL, r_cals[0]);
   rep(i,n-1){
-    n_cal_uppers[i+1]=max(n_cal_uppers[i], n_cals[i+1]);
-    r_cal_uppers[i+1]=max(r_cal_uppers[i], r_cals[i+1]);
+    cals[i+1]=max(cals[i], cals[i+1]);
+    r_cals[i+1]=max(r_cals[i], r_cals[i+1]);
   }
 
-  rep(i,n){
-    ll current_cal = max(n_cals[i], r_cals[i]);
-    result = max(result, current_cal);
-  }
-
+  ll result=0;
+  result = max(result, cals[n-1]);
+  result = max(result, r_cals[n-1]);
   rep(i,n-1){
-    ll current_cal = n_cals[i]+r_cal_uppers[n-1-i-1]-dists[i];
-    ll reverse_cal = r_cals[i]+n_cal_uppers[n-1-i-1]-(c-dists[n-1-i]);
-
-    ll tmp_cal = max(current_cal, reverse_cal);
-    result = max(result, tmp_cal);
+    result = max(result, cals[i]);
+    result = max(result, cals[i]+r_cals[n-2-i]-dists[i]);
+    result = max(result, r_cals[i]);
+    result = max(result, r_cals[i]+cals[n-2-i]-r_dists[i]);
   }
 
   cout<<result<<endl;
 
-//  rep(i,n)cout<<n_cals[i]<<' ';
-//cout<<endl;
-//  rep(i,n)cout<<n_cal_uppers[i]<<' ';
-//cout<<endl;
-
-//  rep(i,n)cout<<r_cals[i]<<' ';
-//cout<<endl;
-//  rep(i,n)cout<<r_cal_uppers[i]<<' ';
+//rep(i,n)cout<<cals[i]<<' ';cout<<endl;
+//rep(i,n)cout<<r_cals[i]<<' ';cout<<endl;
 
   return 0;
 }
--- abc/117/d.cpp
+++ abc/117/d.cpp
@@ -7,28 +7,51 @@ using namespace std;
 
 int main(){
   ll n,k;cin>>n>>k;
-  ll a[n];rep(i,n){cin>>a[i];}
+  ll a[n];
+  rep(i,n)cin>>a[i];
+  int all_bits=40;
+
+//40bit全部見る
+//kの中でbitが立っている箇所を全部見る
+//bitが立っている場所を0にして、その桁数を記憶
+//桁数よりも大きい位置にあるbitはkを真似る
+//同じ桁は0にする
+//それよりも小さい桁はaの同じ桁のビットを見てカウントし、少ない方にする(xがk以下でresultを最大化する値にする)
 
   ll result=0;
-  for(int d=41;d>=-1;d--){
-    if( d!=-1 && !(k & (1LL<<d)) )continue;
+  rep(i,n)result+=(k^a[i]);
 
-    ll tmp=0;
-    for(int e=40;e>=0;e--){
-      ll mask = 1LL<<e;
-      ll num=0;
-      rep(i,n)if(mask & a[i])num++;
+  rep(i,all_bits){
+    ll current=(1LL<<i);
+    if(!(current & k))continue;
 
-      if(e>d){
-        if(k & mask) tmp += mask*(n-num);
-        else tmp += mask*num;
-      }else if(e==d){
-        tmp += mask*num;
-      }else{
-        tmp += mask*max(num, n-num);
+    ll tmp_result=0;
+    rep(j, all_bits){
+      ll mask=1LL<<j;
+      ll count=0;
+      rep(k,n)if( a[k]&mask )count++;
+
+      if(i<j){//もし低減予定のbitよりも高い位置のbitを確認中なら
+        if(k & mask){//そこにbitが立っていたら
+          //xのそこのbitは1になる、よってこの時のXによる結果は数列の当該bitの0の数だけ増加する(つまりn-countだけ増える。
+          tmp_result+=mask*(n-count);
+        }else{
+          //そこのbitが立っていなければxも同じく0になる。
+          //0になるということは数列のうちそこにibtが立っているものの個数だけ増える。のでmask*countとなる
+          tmp_result+=mask*count;
+        }
+      }else if(i==j){//もし低減予定のbitと同一のbitを確認中なら
+        tmp_result+=mask*count;
+      }else if(i>j){//もし低減予定のbitよりも低い位置のbitを確認中なら
+        //可能な限り大きな値にする。
+        tmp_result+=mask*max(count, n-count);
       }
     }
-    result = max(result, tmp);
+
+//cout<<result<<' '<<tmp_result<<endl;
+
+
+    result=max(result, tmp_result);
   }
 
   cout<<result;
