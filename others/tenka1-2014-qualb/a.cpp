1e12fddab0ceb2a9ebd1acbe55f7b9881cd8d4cb
colorbox <colorbox222@gmail.com>
2020-03-22 19:38:56 +0900

    Add regex sample

---
+++ others/tenka1-2014-qualb/a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define rrep(i,n) for(int i = (n)-1; i >= 0; i--)
+#define rep1(i,n) for(int i = 1; i <= (n); i++)
+#define rrep1(i,n) for(int i = (n); i > 0; i--)
+
+#define ll long long
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+
+#define MOD 1000000007
+#define INF 1000000000000000LL
+
+using namespace std;
+
+int main(){
+  string s;cin>>s;
+  string r = regex_replace(s, regex("HAGIYA"), "HAGIXILE");
+  cout<<r<<endl;
+  
+  return 0;
+}
