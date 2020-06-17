a00c03865f086e8565eceb6f42761dadb2fabc9c
colorbox <colorbox222@gmail.com>
2020-06-17 22:55:22 +0900

    Add counting babble sort

---
+++ ant-book/3/3/babble_sort_count.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define rrep(i,n) for(int i = (n)-1; i >= 0; i--)
+#define rep1(i,n) for(int i = 1; i <= (n); i++)
+#define rrep1(i,n) for(int i = (n); i > 0; i--)
+
+#define ll long long
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+
+#define MOD 1000000007
+#define INF 1000000000000000LL
+
+using namespace std;
+
+// Binary Indexed Tree (Fenwick Tree)
+// https://youtu.be/lyHk98daDJo?t=7960
+template<typename T>
+struct BIT {
+  int n;
+  vector<T> d;
+  BIT(int n=0):n(n),d(n+1) {}
+  void add(int i, T x=1) {
+    for (i++; i <= n; i += i&-i) {
+      d[i] += x;
+    }
+  }
+  T sum(int i) {
+    T x = 0;
+    for (i++; i; i -= i&-i) {
+      x += d[i];
+    }
+    return x;
+  }
+};
+
+int main(){
+  int n;cin>>n;
+  vector<int>a(n);
+  rep(i, n)cin>>a[i];
+
+  BIT<int> bit(n+1);
+
+  int result=0;
+  rep(i, n){
+    result += i-bit.sum(a[i]);
+    bit.add(a[i], 1);
+  }
+
+  cout<<result<<endl;
+
+  return 0;
+}
+
+
+/*
+
+4 3 1 4 2
+
+*/
